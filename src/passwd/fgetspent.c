d85f550c9946c96718e97557a3d505d366da9988
Michael Forney <mforney@mforney.org>
2013-11-05 23:26:11 -0800

    shadow: Implement fgetspent

--- src/passwd/fgetspent.c
+++ src/passwd/fgetspent.c
@@ -2,5 +2,10 @@
 
 struct spwd *fgetspent(FILE *f)
 {
-	return 0;
+	static char *line;
+	static struct spwd sp;
+	static size_t size;
+	struct spwd *res = 0;
+	if (getline(&line, &size, f) >= 0 && __parsespent(line, &sp, 0, 0) >= 0) res = &sp;
+	return res;
 }
