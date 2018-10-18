353286984fd3c89a2037c69816b0fe9ae8729733
Michael Forney <mforney@mforney.org>
2018-10-18 12:40:04 -0700

    riscv: Don't define SYS_renameat

--- src/stdio/rename.c
+++ src/stdio/rename.c
@@ -6,7 +6,9 @@ int rename(const char *old, const char *new)
 {
 #ifdef SYS_rename
 	return syscall(SYS_rename, old, new);
-#else
+#elif defined(SYS_renameat)
 	return syscall(SYS_renameat, AT_FDCWD, old, AT_FDCWD, new);
+#else
+	return syscall(SYS_renameat2, AT_FDCWD, old, AT_FDCWD, new, 0);
 #endif
 }
